a74ba69cc5649bc3596ef1e68887fe41d394fcad
etwillbefine <tim.gatzemeier@gmx.de>
2020-06-25 19:08:25 +0200

    add test for successful github webhook validation

--- cmd/helm/webhook_test.go
+++ cmd/helm/webhook_test.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 	"testing"
 	"net/http"
+	"encoding/hex"
 	httptest "net/http/httptest"
 )
 
@@ -47,12 +48,43 @@ func TestWebhookValidateRequestRejectsMissingSignature(t *testing.T) {
 
 	req.Header.Add("x-github-delivery", "x")
 	req.Header.Add("x-github-event", "y")
+	_, err = handler.validateRequest(httptest.NewRecorder(), req)
 	if err == nil || err.Error() != "missing github event signature, webhook event, id or signature is invalid" {
 		t.Error("expected invalid request content error")
 	}
 
-	req.Header.Add("x-github-signature", "sig")
+	req.Header.Add("x-hub-signature", "sig")
+	_, err = handler.validateRequest(httptest.NewRecorder(), req)
 	if err == nil || err.Error() != "missing github event signature, webhook event, id or signature is invalid" {
 		t.Error("expected invalid request content error")
+	}
+}
+
+func TestWebhookValidateRequestSucceeds(t *testing.T) {
+	payload := "request payload"
+	secret := "secret"
+	signatureHmac := signBody([]byte(secret), []byte(payload))
+	signature := make([]byte, hex.EncodedLen(len(signatureHmac)))
+	hex.Encode(signature, signatureHmac)
+
+	reader := strings.NewReader(payload)
+	req := httptest.NewRequest(http.MethodPost, "/", reader)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("x-github-event", "ping")
+	req.Header.Add("x-hub-signature", "sha1=" + string(signature))
+	req.Header.Add("x-github-delivery", "f155be00-b701-11ea-97da-4470faac803a")
+
+	handler := &WebhookHandler{
+		Secret: []byte("secret"),
+	}
+
+	body, err := handler.validateRequest(httptest.NewRecorder(), req)
+	if err != nil {
+		t.Error("expected valid request to succeed: " + err.Error())
+	}
+
+	parsedBody := string(body[:len(body)])
+	if parsedBody != payload {
+		t.Error("wrong content received")
 	}
 }
